bdbf5e6e47f23443ac089d556f2cd3c7010a036f
IsLeony <isavmg21@gmail.com>
2025-04-02 20:41:07 -0300

    notas

--- assets/index.js
+++ assets/index.js
@@ -5,4 +5,9 @@
 /* Editores de texto simples */
 /*Entrada de dados - Processamento de dados - Saída dos dados*/
 /* Variavel - espaço alocados na memória RAM onde guardamos informações por um determinado espaço de tempo*/
-/* */
+/* Só uma informação*/
+/*Dentro do mesmo espaço(scopo), não podemos ter 2 ou mais variaveis do mesmo nome */
+/* var nome  - declara a variavel*/
+/* var idade = 31 */
+/* string, numbers, booleanos*/
+/*CPF é string*/
