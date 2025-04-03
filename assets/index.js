373fe27fe32d8229c69533c35163a948824fd05c
IsLeony <isavmg21@gmail.com>
2025-04-02 21:25:06 -0300

    uuu

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,13 @@
-/*começou dia 2*/
-/*scripts contidos nas paginas do próprio navegador*/
-/* Linguagem de programação */
-/* Manipulação no navegador */
-/* Editores de texto simples */
-/*Entrada de dados - Processamento de dados - Saída dos dados*/
-/* Variavel - espaço alocados na memória RAM onde guardamos informações por um determinado espaço de tempo*/
-/* Só uma informação*/
-/*Dentro do mesmo espaço(scopo), não podemos ter 2 ou mais variaveis do mesmo nome */
-/* var nome  - declara a variavel*/
-/* var idade = 31 */
-/* string, numbers, booleanos*/
+/*começou dia 2
+/*scripts contidos nas paginas do próprio navegador
+/* Linguagem de programação 
+/* Manipulação no navegador 
+/* Editores de texto simples 
+/*Entrada de dados - Processamento de dados - Saída dos dados
+/* Variavel - espaço alocados na memória RAM onde guardamos informações por um determinado espaço de tempo
+/* Só uma informação
+/*Dentro do mesmo espaço(scopo), não podemos ter 2 ou mais variaveis do mesmo nome 
+/* var nome  - declara a variavel
+/* var idade = 31 
+/* string, numbers, booleanos
 /*CPF é string*/
