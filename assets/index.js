3946159f69231a4a55f0fe6a8243adece2e7cb80
IsLeony <isavmg21@gmail.com>
2025-04-02 21:29:49 -0300

    arrumando texto

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,19 @@
 /*começou dia 2
-/*scripts contidos nas paginas do próprio navegador
-/* Linguagem de programação 
-/* Manipulação no navegador 
-/* Editores de texto simples 
-/*Entrada de dados - Processamento de dados - Saída dos dados
-/* Variavel - espaço alocados na memória RAM onde guardamos informações por um determinado espaço de tempo
-/* Só uma informação
-/*Dentro do mesmo espaço(scopo), não podemos ter 2 ou mais variaveis do mesmo nome 
-/* var nome  - declara a variavel
-/* var idade = 31 
-/* string, numbers, booleanos
-/*CPF é string*/
+scripts contidos nas paginas do próprio navegador
+ Linguagem de programação 
+ Manipulação no navegador 
+ Editores de texto simples 
+Entrada de dados - Processamento de dados - Saída dos dados
+ Variavel - espaço alocados na memória RAM onde guardamos informações por um determinado espaço de tempo
+ Só uma informação
+Dentro do mesmo espaço(scopo), não podemos ter 2 ou mais variaveis do mesmo nome 
+ var nome  - declara a variavel
+ var idade = 31 
+ string, numbers, booleanos
+CPF é string
+%modulo = resto da divisão
+
+
+
+
+*/
